801673a9df4be32d9561aab80a4e8c0ac90d09b7
Nathanyel Calero <ncalero@ucsd.edu>
2018-03-01 10:51:11 -0800

    Submit

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,18 @@
 function initMap() {
-	// add your code here
+  // Create center marker at UCSD
+  var ucsd_ltlng = {
+    lat: 32.878827,
+    lng: -117.236043,
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_ltlng,
+    zoom: 15,
+  });
+
+  var marker = new google.maps.Marker({
+    position: ucsd_ltlng,
+    map: map,
+    title: 'UCSD',
+  });
 }
